045eab01b6ce314d437338a97e4668190cd799ae
Jishnu Saurav Mittapalli <55013611+jishnusaurav@users.noreply.github.com>
2020-08-13 23:44:07 +0530

    Create identical_trees.cpp

---
+++ Easy/identical_trees.cpp
@@ -0,0 +1,10 @@
+bool isIdentical(Node *r1, Node *r2)
+{
+    //Your Code here
+    if(r1==NULL and r2==NULL)
+    return true;
+    if (r1 != NULL && r2 != NULL)  {
+        return ((r1->data==r2->data) && isIdentical(r1->right,r2->right) && isIdentical(r1->left,r2->left));
+    }
+    return false;
+}
